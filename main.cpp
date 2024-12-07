d851d99d600cfe1a56ce78a4f401298eae8d953d
unknown <ahmed@ahmed>
2024-12-07 14:34:32 +0200

    step three 'Adding Jumping' to the Dion add function: 	void keyboard(char,int,int); 	void update(int);

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include<GL/freeglut.h>
+#include<iostream>
+using namespace std;
+
+
+// Global variables for dino's position and jump state
+float dinoY = 100.0f;
+float velocity = 0.0f;
+bool isJumping =false;
+float windowWidth = 800.0f;
+float windowHeight = 600.0f;
+
+// obstacle variables
+float obstacleX      = windowWidth;
+float obstacleWidth  = 30.0f;
+float obstacleHeight = 50.0f;
+float obstacleSpeed  = 5.0f;
 
 void drawGround(){
 
   glColor3f(0.0,0.8,0.0);
   glBegin(GL_QUADS);
-    glVertex2f(0.0,50.0);
-    glVertex2f(800.0, 50.0);
-    glVertex2f(800.0, 0.0);
-    glVertex2f(0.0, 0.0);
+    glVertex2f(0.0,50.0);             // bottom-left corner
+    glVertex2f(windowWidth, 50.0);    // bottom-right corner
+    glVertex2f(windowWidth, 0.0);     // top-right corner
+    glVertex2f(0.0, 0.0);             // top-left corner
   glEnd();
 
 }
@@ -15,12 +31,43 @@ void drawDino(){
 
   glColor3f(0.8,0.3,0.0);
   glBegin(GL_QUADS);
-    glVertex2f(100.0f, 150.0f);
-    glVertex2f(130.0f, 150.0f);
-    glVertex2f(130.0f, 100.0f);
-    glVertex2f(100.0f, 100.0f);
+    glVertex2f(100.0f, dinoY + 50.0f);
+    glVertex2f(130.0f, dinoY + 50.0f);
+    glVertex2f(130.0f, dinoY);
+    glVertex2f(100.0f, dinoY);
   glEnd();
 
+}
+
+
+void keyboard(unsigned char key, int x,int y){
+
+  if(key == 32 && !isJumping){
+    isJumping = true;
+    velocity = 10.0f;
+  }
+
+}
+
+void update(int value){
+
+  if(isJumping){
+
+    velocity -= 0.5f;
+
+    dinoY += velocity;
+
+    cout<<dinoY<<velocity<<endl;
+
+    if(dinoY <= 100.0f){
+      dinoY = 100.0f;
+      isJumping = false;
+      velocity = 0.0f;
+    }
+  }
+
+  glutPostRedisplay();
+  glutTimerFunc(16, update, 0);
 }
 
 void display(){
@@ -37,6 +84,9 @@ void display(){
 }
 
 void reshape(int wigth, int height){
+
+  windowWidth  = wigth;
+  windowHeight = height;
 
   glViewport(0,0,wigth,height);
   glMatrixMode(GL_PROJECTION);
@@ -59,6 +109,8 @@ int main(int argc, char** argv){
   //set up callbacks
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
+  glutKeyboardFunc(keyboard);
+  glutTimerFunc(25, update, 0);
 
 
   glutMainLoop();
